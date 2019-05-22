115ae287994f752aa65ed1ae3514adfe8c1ad563
Kevin <kevin.pason95@gmail.com>
2019-05-22 12:28:41 +0200

    Adding Tables 3

--- db/migrate/20190522100642_categories_items.rb
+++ db/migrate/20190522100642_categories_items.rb
@@ -2,7 +2,7 @@ class CategoriesItems < ActiveRecord::Migration[5.2]
   def change
     create_table :categories_id do |t|
       t.integer :item_id
-      t.integer :categories_id
+      t.integer :category_id
     end
   end
 end
