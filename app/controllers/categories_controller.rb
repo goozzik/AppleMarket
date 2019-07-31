6a2499a94e03395dbb1f0db1fd45bfb279b61bac
lorddraagonix <kevin.pason95@gmail.com>
2019-07-31 16:45:12 +0200

    fixing

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,23 +1,12 @@
 class CategoriesController < ApplicationController
 
   def index
-<<<<<<< HEAD
-<<<<<<< HEAD
-  end
-  
-=======
-=======
->>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
     @categories = Category.all
   end
 
   # GET
   def show
-    
+    @category = Category.find(params[:id])
   end
 
-<<<<<<< HEAD
->>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
-=======
->>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,10 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-  get 'categories/index'
-
-  resources :categories
-
-  root 'categories#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-=======
->>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
   get 'categories/:id/items' => 'categories#show'
   resources :items
-#@category = Category.find(params[:id])
+
   get 'categories/index'
   resources :categories
 
-
   root 'categories#index'
-<<<<<<< HEAD
->>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
-=======
->>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
-#root 'application#welcome'
-end
+end 
