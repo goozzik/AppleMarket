033d76ead805eec25b75f334f05f0f62c75dc8d3
lorddraagonix <kevin.pason95@gmail.com>
2019-07-31 16:06:45 +0200

    Add index + show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,9 +2,12 @@ class CategoriesController < ApplicationController
 
   def index
 <<<<<<< HEAD
+<<<<<<< HEAD
   end
   
 =======
+=======
+>>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
     @categories = Category.all
   end
 
@@ -13,5 +16,8 @@ class CategoriesController < ApplicationController
     
   end
 
+<<<<<<< HEAD
+>>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
+=======
 >>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 
   get 'categories/index'
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   root 'categories#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 =======
+=======
+>>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
   get 'categories/:id/items' => 'categories#show'
   resources :items
 #@category = Category.find(params[:id])
@@ -17,6 +20,9 @@ Rails.application.routes.draw do
 
 
   root 'categories#index'
+<<<<<<< HEAD
+>>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
+=======
 >>>>>>> 9325dfc... Add Categories index page + Categories show page + seeds
 #root 'application#welcome'
 end
